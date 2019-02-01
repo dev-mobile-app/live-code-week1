22a7686fbe44dd1b17a2c40068b7dc4696296b9a
Elfin Sanjaya <45805396+elfinsanjaya12@users.noreply.github.com>
2019-02-01 08:32:25 +0700

    Create 002.js

---
+++ 002.js
@@ -0,0 +1,28 @@
+// Competencies: Loopings
+/*
+=============
+Number Ladder
+=============
+Instruksi
+=========
+Buatlah sebuah proses beberapa penghitungan dengan menggunakan looping (boleh dengan while atau for)
+Disediakan variable height. 
+Buatlah sebuah tangga angka dengan pola berikut:
+contoh 1 (height = 3):
+1
+12
+123
+contoh 2 (height = 5):
+1
+12
+123
+1234
+12345
+ 
+contoh 3 (height = 1):
+1
+tinggi tangga sesuai dengan nilai variable height, dan isi dari tangga adalah angka dari height itu sendiri.
+Ketentuan
+=========
+Wajib menggunakan looping!
+*/
